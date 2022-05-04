5137319484d8e2cef367c20d09050c6b61543676
Askarunro <paskar.y.a@gmail.com>
2022-05-04 21:56:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,40 +8,42 @@ import MovieDetailsPage from "./components/MovieDetailsPage";
 import Cast from "./components/Cast/Cast";
 import Reviews from "./components/Reviews";
 import MoviesPage from "./components/MoviesPage";
-import Layout from "./components/Layout"
+import Layout from "./components/Layout";
 import { useEffect, useState } from "react";
-// import axios from "axios";
-
-
+import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const KEY = "api_key=5d104e06eaa9cbca519409c6ed30e31b";
 
-// axios.defaults.baseURL =
-//   "https://api.themoviedb.org/3/movie/550?api_key=5d104e06eaa9cbca519409c6ed30e31b";
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
 
 function App() {
   const [trend, setTrend] = useState([]);
+  // useEffect(() => {
+  //   fetch(`${BASE_URL}/trending/all/week?${KEY}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json) => setTrend(json.results));
+  // }, []);
   useEffect(() => {
-    fetch(`${BASE_URL}/trending/all/week?${KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => setTrend(json.results));
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/trending/all/week?${KEY}&page=1`);
+        console.log(response.data.results);
+        setTrend(response.data.results);
+      } catch {
+        console.log("error");
+      }
+    };
+
+    fetchData();
   }, []);
-
-
-const handleTrendItem=(data)=>{
-  // console.log(data)
-  // console.log(trend.find(item=>item.id==="data"))
-  // const test = trend.find(data)
-}
 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            {/* <Route index element={<HomePage trend={trend} handleTrendItem={handleTrendItem} />}></Route> */}
             <Route index element={<HomePage trend={trend} />}></Route>
             <Route path="/movies" element={<MoviesPage />}></Route>
             <Route path=":movieId" element={<MovieDetailsPage trend={trend} />}>
