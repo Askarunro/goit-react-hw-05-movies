5fa8766b225a5995e26b773ef1b09a949146233b
Askarunro <paskar.y.a@gmail.com>
2022-05-18 15:11:24 +0300

    Update Reviews.js

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,6 +1,29 @@
-const Reviews=()=>{
-    return(
-        <h1>Reviews</h1>
-    )
-}
-export default Reviews
+import { useParams } from "react-router";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import * as API from "..//..//services/movies-api";
+import { useEffect, useState } from "react";
+import r from "./Reviews.module.css";
+
+const Reviews = () => {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    API.fetchMovieReviews(movieId).then(setReviews);
+  }, []);
+
+  return (
+    <>
+    {!reviews[1] && <h2>We don`t have any reviews for hies movie</h2>}
+    {reviews[1] && <ul className={r.list}>
+      {reviews.map((review) => (
+        <li key={review.id} className={r.item}>
+          <h2>Author: {review.author}</h2>
+          <p>{review.content}</p>
+        </li>
+      ))}
+    </ul>}
+    </>
+  );
+};
+export default Reviews;
