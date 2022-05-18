9d92624ba63af510835edf29387c8fa55ed7d941
Askarunro <paskar.y.a@gmail.com>
2022-05-18 15:10:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { lazy } from "react";
-// import HomePage from "./components/HomePage";
-import MovieDetailsPage from "./components/MovieDetailsPage";
-import Cast from "./components/Cast/Cast";
-import Reviews from "./components/Reviews";
-import MoviesPage from "./components/MoviesPage";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Layout from "./components/Layout";
 // import { useEffect, useState } from "react";
 // import axios from "axios";
@@ -14,21 +9,38 @@ import Layout from "./components/Layout";
 // const KEY = "api_key=5d104e06eaa9cbca519409c6ed30e31b";
 
 // axios.defaults.baseURL = "https://api.themoviedb.org/3/";
-const  HomePage = lazy(()=>import('./components/HomePage'))
+const HomePage = lazy(() => {
+  return import("./components/HomePage");
+});
+const MovieDetailsPage = lazy(() => {
+  return import("./components/MovieDetailsPage");
+});
+const MoviesPage = lazy(() => {
+  return import("./components/MoviesPage");
+});
+
+const Reviews = lazy(() => {
+  return import("./components/Reviews");
+});
+const Cast = lazy(() => {
+  return import("./components/Cast");
+});
 function App() {
   return (
-    <>
+    <Suspense fallback={<div>Loading</div>}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />}></Route>
           <Route path="/movies" element={<MoviesPage />}></Route>
-          <Route path=":movieId" element={<MovieDetailsPage />}>
-            <Route path=":cast" element={<Cast />} />
-            <Route path=":reviews" element={<Reviews />} />
+          <Route  path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
+          {/* <Route path='*' element={<Navigate to='/'/>}></Route> */}
+          <Route path='*' element={<div><h2>Oops we have a problem</h2><h3>Pages not found</h3></div>}></Route>
         </Route>
       </Routes>
-    </>
+    </Suspense>
   );
 }
 
