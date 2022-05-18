15f89bb3e0e8e5062571dbf8b15893ffd33c949e
Askarunro <paskar.y.a@gmail.com>
2022-05-18 15:11:09 +0300

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,30 @@
 import { Outlet } from "react-router";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import backImg from "./back.png";
+import l from "./Layout.module.css";
 
 const Layout = () => {
-  
-    // console.log(trend.data)
-    return (
-      <>
-        <nav>
-          <Link to="">HomePage</Link>
-          <Link to="movies">Movies</Link>
-        </nav>
-        <hr/>
-        <Outlet/>
-      </>
-    );
-  };
+  // console.log(trend.data)
+  return (
+    <>
+      <nav className={l.navigate}>
+        <NavLink to="" className={l.link}>
+          HomePage
+        </NavLink>
+        <NavLink to="movies" className={l.link}>
+          Movies
+        </NavLink>
+      </nav>
+      <hr />
+      <Link to="/">
+        <div className={l.wrapper}>
+          <img src={backImg} alt={`go back`} width={30} />
+          <p>Go back</p>
+        </div>
+      </Link>
+      <Outlet />
+    </>
+  );
+};
 
-  export default Layout
-
+export default Layout;
